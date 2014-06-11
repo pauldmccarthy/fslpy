f355151f0a52f9cbea9440a040f802ff0e5a195e
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-06-11 10:10:26 +0100

    Virtualenv is set up properly, so I can run pdb inside emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,6 @@
-((python-mode . ((project-venv-name . "pyfsl-env"))))
+((python-mode . ((project-venv-name . "pyfsl-env")))
+
+ ; make our bodgy wx widgets installation work nicely with emacs
+ (nil . ((eval . (setenv "DYLD_LIBRARY_PATH" "/Users/paulmc/optlib/wx3.0.0/usr/local/lib/"))))
+ (nil . ((eval . (setenv "PYTHONPATH" "/Users/paulmc/optlib/wx3.0.0/lib/python2.7/site-packages:/Users/paulmc/optlib/wx3.0.0/lib/python2.7/site-packages/wx-3.0-osx_carbon"))))
+ (nil . ((eval . (setenv "PYTHONHOME" "/Users/paulmc/.virtualenvs/pyfsl-env")))))
