f464655c51e0c132d5af9da15e2718bba401d9dc
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-06-30 17:03:23 +0100

    ImageListPanel is now less closely coupled to ImageList - display panels are added/removed in reaction to an image list change, instead of being performed alongside local changes to the image list. This is so that the ImageListPanel will behave nicely in reaction to changes to the image list from external sources (e.g. File -> Add image).

--- fsl/fslview/imagelistpanel.py
+++ fsl/fslview/imagelistpanel.py
@@ -2,93 +2,132 @@
 #
 # imagelistpanel.py - A panel which displays an image list, and a 'console'
 # allowing the display properties of each image to be changed, and images
-# to be added/removed from the list.  See fsl.data.fslimage.ImageList.
+# to be added/removed from the list. 
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
+"""A panel which displays an image list, and a 'console' allowing the
+display properties of each image to be changed, and images
+to be added/removed from the list.  See :class:`~fsl.data.fslimage.ImageList`.
+"""
 
 import logging
 log = logging.getLogger(__name__)
 
-import os
 import os.path as op
 
 import wx
 
-import fsl.data.fslimage  as fslimage
-import fsl.data.imagefile as imagefile
-
-import pwidgets.elistbox  as elistbox
+import pwidgets.elistbox as elistbox
 import props
 
 
 class ImageListPanel(wx.Panel):
-    """
-    A panel which contains a list box displaying the list of loaded images
+    """A panel which contains a list box displaying the list of loaded images
     and a control panel allowing the display properties of the currently
     selected image to be modified.  The list box allows the image order
     to be changed, and allows images to be added and removed from the list.
     """
+
     
     def __init__(self, parent, imageList):
-        """
-        Creates and lays out an ImageListPanel.
+        """Create and lay out an :class:`ImageListPanel`.
+
+        :param parent:    The :mod:`wx` parent object.
+        :param imageList: A :class:`~fsl.data.fslimage.ImageList` instance.
         """
         
         wx.Panel.__init__(self, parent)
-        self.imageList = imageList
+        self._imageList = imageList
 
-        imageNames = [img.name      for img in imageList]
-        imagePaths = [img.imageFile for img in imageList]
-        imagePaths = map(op.abspath, imagePaths)
+        self._name = '{}_{}'.format(self.__class__.__name__, id(self))
 
-        # list box containing the list of images
-        self.listBox = elistbox.EditableListBox(
+        # list box containing the list of images - it 
+        # is populated in the _imageListChanged method
+        self._listBox = elistbox.EditableListBox(
             self,
-            imageNames,
-            imageList,
-            tooltips=imagePaths,
             style=elistbox.ELB_REVERSE | elistbox.ELB_TOOLTIP)
 
-        self.listBox.Bind(elistbox.EVT_ELB_SELECT_EVENT, self._imageSelected)
-        self.listBox.Bind(elistbox.EVT_ELB_MOVE_EVENT,   self._imageMoved)
-        self.listBox.Bind(elistbox.EVT_ELB_REMOVE_EVENT, self._imageRemoved)
-        self.listBox.Bind(elistbox.EVT_ELB_ADD_EVENT,    self._addImage)
+        # listeners for when the user does
+        # something with the list box
+        self._listBox.Bind(elistbox.EVT_ELB_SELECT_EVENT, self._imageSelected)
+        self._listBox.Bind(elistbox.EVT_ELB_MOVE_EVENT,   self._imageMoved)
+        self._listBox.Bind(elistbox.EVT_ELB_REMOVE_EVENT, self._imageRemoved)
+        self._listBox.Bind(elistbox.EVT_ELB_ADD_EVENT,    self._addImage)
 
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.SetSizer(self.sizer)
+        self._sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.SetSizer(self._sizer)
 
-        self.sizer.Add(self.listBox, flag=wx.EXPAND, proportion=1)
+        self._sizer.Add(self._listBox, flag=wx.EXPAND, proportion=1)
 
-        # a panel for each image, containing widgets
-        # allowing the image display properties to be
-        # changed
-        for i, image in enumerate(imageList):
-            self._makeDisplayPanel(image)
-
-        self.imageList.addListener(
+        self._imageList.addListener(
             'selectedImage',
-            '{}_{}'.format(self.__class__.__name__, id(self)),
+            self._name,
             self._selectedImageChanged)
+        
+        self._imageList.addListener(
+            'images',
+            self._name,
+            self._imageListChanged)
 
-        self.imageList.selectedImage = len(imageList) - 1
+        # This dictionary contains {id(image) -> display panel}
+        # mappings for all display panels that that currently exist.
+        # It is maintained by the _imageListChanged method.
+        self._displayPanels = {}
+
+        self._imageListChanged()
+        self._selectedImageChanged()
 
         self.Layout()
 
         
+    def _imageListChanged(self, *a):
+        """Called when the image list changes. Destroys and/or creates
+        display panels as necessary, for images which have been
+        added/removed to/from the list.
+        """
+
+        imgIds = map(id, self._imageList)
+
+        # First check to see if there are any display panels
+        # for which the corresponding image is no longer
+        # present in the list.
+        for imgId, displayPanel in self._displayPanels.items():
+
+            if imgId not in imgIds:
+                self._sizer.Detach(displayPanel)
+                displayPanel.Destroy()
+                self._displayPanels.pop(imgId)
+        
+        # Now check to see if any images have been added,
+        # and we need to create a display panel for them
+        for i, image in enumerate(self._imageList):
+
+            # check to see if a display
+            # panel exists for this image
+            try: self._displayPanels[id(image)]
+
+            # if one doesn't, make one and 
+            # add the image to the list box
+            except KeyError:
+                self._makeDisplayPanel(image)
+                self._listBox.Append(image.name,
+                                     image,
+                                     op.abspath(image.imageFile))
+
+        
     def _selectedImageChanged(self, *a):
+        """Called when the :attr:`~fsl.data.fslimage.ImageList.selectedImage`
+        property changes. Shows the display panel for the newly selected
+        image.
         """
-        Called when the ImageList.selectedImage property changes.
-        Shows the control panel for the currently selected image.
-        """
-        self._showDisplayPanel(self.imageList.selectedImage)
+        self._showDisplayPanel(self._imageList.selectedImage)
 
         
     def _makeDisplayPanel(self, image):
-        """
-        Creates a panel containing widgets allowing the user to
+        """Creates a panel containing widgets allowing the user to
         edit the display properties of the given image. A reference
-        to the panel is added as an attribute of the image.
+        to the panel is stored in the :attr:`_displayPanels` dict.
         """
 
         parentPanel = wx.Panel(self)
@@ -103,116 +142,85 @@ class ImageListPanel(wx.Panel):
 
         parentSizer.Layout()
         
-        self.sizer.Add(parentPanel, flag=wx.EXPAND, proportion=2)
-        image.setAttribute(
-            'displayPanel_{}'.format(id(self)), parentPanel)
+        self._sizer.Add(parentPanel, flag=wx.EXPAND, proportion=2)
+
+        self._displayPanels[id(image)] = parentPanel
         return parentPanel
-
-        
-    def _imageMoved(self, ev):
-        """
-        Called when an image name is moved in the ListBox. Reorders the
-        ImageList to reflect the change.
-        """
-
-        self.imageList.move(ev.oldIdx, ev.newIdx)
-        self.Refresh()
-
-        
-    def _imageSelected(self, ev):
-        """
-        Called when an image is selected in the ListBox. Displays the
-        corresponding image display configuration panel.
-        """
-        self.imageList.selectedImage = ev.idx
 
 
     def _showDisplayPanel(self, idx):
-        """
-        Shows the display panel for the image at the specified index.
-        """
+        """Shows the display panel for the image at the specified index."""
         
-        for i, image in enumerate(self.imageList):
-            
-            displayPanel = image.getAttribute( 
-                'displayPanel_{}'.format(id(self)))
+        for i, image in enumerate(self._imageList):
 
+            displayPanel = self._displayPanels[id(image)]
+            
             if i == idx:
-                log.debug('Showing display panel '
-                          'for image {}'.format(self.imageList[i].name))
+                log.debug('Showing display panel for '
+                          'image {}'.format(image.name))
             
             displayPanel.Show(i == idx)
 
-        self.listBox.SetSelection(idx)
-        self.Layout()
-        self.Refresh()
-
-        
-    def _addImage(self, ev, lastDir=None):
-        """
-        Called when the 'add' button on the list box is pressed. Pops up an
-        open file dialog prompting the user to choose one or more image files,
-        then opens those files, adds them to the image list, and creates
-        display panels for each of them.
-        """
-
-        if lastDir is None:
-            try:    lastDir = self._lastDir
-            except: lastDir = os.getcwd()
-
-        wildcard = imagefile.wildcard()
-
-        # TODO wx wildcard handling is buggy,
-        # so i'm disabling it for now
-        dlg = wx.FileDialog(self.GetParent(),
-                            message='Open image file',
-                            defaultDir=lastDir,
-                            # wildcard=wildcard,
-                            style=wx.FD_OPEN | wx.FD_MULTIPLE)
-
-        if dlg.ShowModal() != wx.ID_OK: return
-
-        paths = dlg.GetPaths()
-
-        for path in paths:
-            image = fslimage.Image(path)
-            self.imageList.append(image)
-            self.listBox.Append(image.name, image, tooltip=image.imageFile)
-
-            self._makeDisplayPanel(image)
-
-        self.imageList.selectedImage = len(self.imageList) - 1
-        
-        # This panel may have changed size, so
-        # tell the parent to lay itself out
+        self._listBox.SetSelection(idx)
         self.GetParent().Layout()
         self.GetParent().Refresh()
+
+        
+    def _imageMoved(self, ev):
+        """Called when an image name is moved in the
+        :class:`~pwidgets.elistbox.EditableListBox`. Reorders the
+        :class:`~fsl.data.fslimage.ImageList` to reflect the change.
+        """
+        self._imageList.move(ev.oldIdx, ev.newIdx)
+
+        
+    def _imageSelected(self, ev):
+        """Called when an image is selected in the
+        :class:`~pwidgets.elistbox.EditableListBox`. Displays the
+        corresponding image display panel.
+        """
+        self._imageList.selectedImage = ev.idx
+
+        
+    def _addImage(self, ev):
+        """Called when the 'add' button on the list box is pressed.
+
+        Pops up an open file dialog prompting the user to choose one or more
+        image files, then opens those files, adds them to the image list, and
+        creates display panels for each of them.  See the
+        :meth:`~fsl.data.fslimage.ImageList.addImages` method.
+        """
+        self._imageList.addImages()
+
+        # Tell the parent window to lay itself out in
+        # case the image list was 0, and is now non-0,
+        # in which case  this ImageListPanel will have
+        # changed size.
+        if len(self._imageList) > 0:
+            self.GetParent().Layout()
+            self.GetParent().Refresh()
 
 
     def _imageRemoved(self, ev):
         """
         Called when an item is removed from the image listbox. Removes the
-        corresponding image from the image list, deletes its display panel,
-        and refreshes the list panel.
+        corresponding image from the image list. The corresponding display
+        panel will removed via the :meth:`~_imageListChanged` method, which
+        is registered as a listener on the
+        :attr:`~fsl.data.fslimage.ImageList.images` list.
         """
+        self._imageList.pop(ev.idx)
 
-        image        = self.imageList.pop(ev.idx)
-        displayPanel = image.getAttribute('displayPanel_{}'.format(id(self)))
+        # if the image list is now empty, there are no more
+        # display panels, and hence the size of this
+        # ImageListPanel will have changed. So tell the parent
+        # to lay itself out again
+        if len(self._imageList) == 0:
+            self.GetParent().Layout()
+            self.GetParent().Refresh()
 
-        displayPanel.Destroy()
-
-        # make sure that an image is selected
-        if len(self.imageList) > 0:
-            
-            self.imageList.selectedImage = self.listBox.GetSelection()
-
-            # If the image list was of length 2, and image 0 was
-            # previously selected, the above assignment will not
-            # change the selectedImage property value, and our
-            # listener function will not be called. So here we're
-            # just making sure that it does get called
-            if len(self.imageList) == 1:
-                self._selectedImageChanged()
-
-        self.GetParent().Layout()
-        self.GetParent().Refresh()
+        # Even though the image list has changed, the selected
+        # image index may not have changed. So we make doubly
+        # sure that the correct display panel is visible
+        else:
+            self._selectedImageChanged()
