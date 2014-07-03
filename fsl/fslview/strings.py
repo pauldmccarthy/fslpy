8d3d32bacc95a21fac5adcbbb9ec65893676218c
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-07-03 12:06:39 +0100

    New module, strings.py, which contains strings used in fslview interface.

---
+++ fsl/fslview/strings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# strings.py - Labels used throughout FSLView.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+orthoTitle        = 'Ortho view'
+lightBoxTitle     = 'Lightbox view'
+imageDisplayTitle = 'Image display properties'
+imageListTitle    = 'Image list'
+locationTitle     = 'Cursor location'
+
+openFile = 'Add image file'
+openStd  = 'Add standard'
