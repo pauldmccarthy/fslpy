27fa1ec8f550f759c543d5be65659d762afe808c
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-09-18 16:53:48 +0100

    Colour map stuff documentation

--- fsl/fslview/colourmaps.py
+++ fsl/fslview/colourmaps.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 #
-# colourmaps.py -
+# colourmaps.py - Manage colour maps for image rendering.
 #
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
+"""Module which manages the colour maps available for image rendering.
+
+When this module is first initialised, it searches in the
+``fsl/fslview/colourmaps/`` directory, and attempts to load all files within
+which have the suffix ``.cmap``. Such a file is assumed to contain a list of
+RGB colours, one per line, with each colour specified by three space-separated
+floating point values in the range 0.0 - 1.0.
+
+This list of RGB values is used to create a
+:class:`matplotlib.colors.ListedColormap` object, which is then registered
+with the :mod:`matplotlib.cm` module (using the file name prefix as the colour
+map name), and thus made available for rendering purposes.
+
+In addition to these custom `.cmap` colour maps, a handful of built-in
+matplotlib colour maps are also made available.
+
+This module provides two attributes:
+
+  - :data:`default`:   The name of the colour map to be used for new images.
+
+  - :data:`cmapNames`: A list of all colour maps which should be used for
+                       rendering images.
+"""
 
 import os.path as op
 import glob
@@ -37,6 +60,7 @@ cmapNames = ['Greys_r',
              'copper_r']
 
 
+# Load all custom colour maps from the colourmaps/*.cmap files.
 for cmapFile in glob.glob(op.join(op.dirname(__file__),
                                   'colourmaps',
                                   '*.cmap')):
