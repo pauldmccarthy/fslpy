f03660a124fa2a62716f6277d2fc5149af4aa228
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-10-07 16:27:51 +0100

    Sample grid space logic is now in globject module, so it can be used by modules other than glimage.

--- fsl/fslview/gl/glimage.py
+++ fsl/fslview/gl/glimage.py
@@ -46,6 +46,7 @@ log = logging.getLogger(__name__)
 import numpy          as np
 import OpenGL.GL      as gl
 import fsl.fslview.gl as fslgl
+import                   globject
 
 class GLImage(object):
     """The :class:`GLImage` class encapsulates the data and logic required to
@@ -230,61 +231,9 @@ def genVertexData(image, display, xax, yax):
     :arg yax:     The world space axis which corresponds to the
                   vertical screen axis (0, 1, or 2). 
     """
-
-    zax        = 3 - xax - yax
-    worldRes   = display.worldResolution
-    voxelRes   = display.voxelResolution
-    transform  = display.transform
-
-    # These values give the min/max x/y values
-    # of a bounding box which encapsulates
-    # the entire image
-    xmin, xmax = image.imageBounds(xax)
-    ymin, ymax = image.imageBounds(yax)
-
-    # The width/height of a displayed voxel.
-    # If we are displaying in real world space,
-    # we use the world display resolution
-    if transform in ('affine'):
-
-        xpixdim = worldRes
-        ypixdim = worldRes
-
-    # But if we're just displaying the data (the
-    # transform is 'id' or 'pixdim'), we display
-    # it in the resolution of said data.
-    else:
-        xpixdim = image.pixdim[xax] * voxelRes
-        ypixdim = image.pixdim[yax] * voxelRes
-
-    # Number of samples across each dimension,
-    # given the current sample rate
-    xNumSamples = np.floor((xmax - xmin) / xpixdim)
-    yNumSamples = np.floor((ymax - ymin) / ypixdim)
-
-    # the adjusted width/height of our sampled voxels
-    xpixdim = (xmax - xmin) / xNumSamples
-    ypixdim = (ymax - ymin) / yNumSamples
-
-    log.debug('Generating coordinate buffers for {} '
-              '({} resolution {}/{}, num samples {})'.format(
-                  image.name, transform, worldRes, voxelRes,
-                  xNumSamples * yNumSamples))
-
-    # The location of every displayed
-    # voxel in real world space
-    worldX = np.linspace(xmin + 0.5 * xpixdim,
-                         xmax - 0.5 * xpixdim,
-                         xNumSamples)
-    worldY = np.linspace(ymin + 0.5 * ypixdim,
-                         ymax - 0.5 * ypixdim,
-                         yNumSamples)
-
-    worldX, worldY = np.meshgrid(worldX, worldY)
-
-    worldX  = worldX.flatten()
-    worldY  = worldY.flatten()
-    nVoxels = len(worldX)
+    
+    worldCoords, xpixdim, ypixdim = globject.calculateSamplePoints(
+        image, display, xax, yax)
 
     # The geometry of a single
     # voxel, rendered as a quad
@@ -297,31 +246,21 @@ def genVertexData(image, display, xax, yax):
     voxelGeom[:, 0] *= xpixdim
     voxelGeom[:, 1] *= ypixdim
 
-    worldX = worldX.repeat(4) 
-    worldY = worldY.repeat(4)
-    worldZ = np.zeros(len(worldX))
-
-    worldCoords = [None] * 3
-    texCoords   = [None] * 3
+    nVoxels     = worldCoords.shape[0]
+    worldCoords = worldCoords.repeat(4, 0)
 
     # The texture coordinates are in the centre of each
     # set of 4 vertices, so allow us to look up the
-    # colour for every vertex coordinate
-    texCoords[xax] = worldX
-    texCoords[yax] = worldY
-    texCoords[zax] = worldZ
+    # colour for every vertex coordinate 
+    texCoords = np.copy(worldCoords)
 
     # The world coordinates define a bunch of sets
     # of 4 vertices, each rendered as a quad
-    worldX = worldX + np.tile(voxelGeom[:, 0], nVoxels)
-    worldY = worldY + np.tile(voxelGeom[:, 1], nVoxels)
+    worldCoords[:, xax] += np.tile(voxelGeom[:, 0], nVoxels)
+    worldCoords[:, yax] += np.tile(voxelGeom[:, 1], nVoxels)
 
-    worldCoords[xax] = worldX
-    worldCoords[yax] = worldY
-    worldCoords[zax] = worldZ
-
-    worldCoords = np.array(worldCoords, dtype=np.float32).transpose()
-    texCoords   = np.array(texCoords,   dtype=np.float32).transpose()
+    worldCoords = np.array(worldCoords, dtype=np.float32)
+    texCoords   = np.array(texCoords,   dtype=np.float32)
 
     return worldCoords, texCoords
 
--- fsl/fslview/gl/globject.py
+++ fsl/fslview/gl/globject.py
@@ -18,16 +18,108 @@ GL Objects must have the following methods:
 import logging
 log = logging.getLogger(__name__)
 
+import numpy                       as np
 
-import fsl.fslview.gl.glimage  as glimage
-import fsl.fslview.gl.glcircle as glcircle
+import fsl.fslview.gl.glimage      as glimage
+import fsl.fslview.gl.glcircle     as glcircle
+import fsl.fslview.gl.gltensorline as gltensorline
+
+
+_objectmap = {
+    'volume' : glimage     .GLImage,
+    'circle' : glcircle    .GLCircle,
+    'tensor' : gltensorline.GLTensorLine
+}
 
 def createGLObject(image, display):
 
-    if   display.imageType == 'volume':
-        return glimage.GLImage(image, display)
-    elif display.imageType == 'circle':
-        return glcircle.GLCircle(image, display)
-        
-    elif display.imageType == 'tensor':
-        pass 
+    ctr = _objectmap.get(display.imageType, None)
+
+    if ctr is not None: return ctr(image, display)
+    else:               return None
+
+
+def calculateSamplePoints(image, display, xax, yax):
+    """Calculates a uniform grid of points, in real world space along the x-y
+    plane (as specified by the xax/yax indices), at which the given image
+    should be sampled for display purposes.
+
+    This function returns a tuple containing:
+
+     - a numpy array of shape `(N, 3)`, containing the coordinates of the centre
+       of every sampling point in real worl space.
+
+     - the horizontal distance (along xax) between adjacent points
+
+     - the vertical distance (along yax) between adjacent points
+
+    :arg image:   The :class:`~fsl.data.image.Image` object to
+                  generate vertex and texture coordinates for.
+
+    :arg display: A :class:`~fsl.fslview.displaycontext.ImageDisplay`
+                  object which defines how the image is to be
+                  rendered.
+
+    :arg xax:     The world space axis which corresponds to the
+                  horizontal screen axis (0, 1, or 2).
+
+    :arg yax:     The world space axis which corresponds to the
+                  vertical screen axis (0, 1, or 2).
+
+    """
+
+    worldRes   = display.worldResolution
+    voxelRes   = display.voxelResolution
+    transform  = display.transform
+
+    # These values give the min/max x/y values
+    # of a bounding box which encapsulates
+    # the entire image
+    xmin, xmax = image.imageBounds(xax)
+    ymin, ymax = image.imageBounds(yax)
+
+    # The width/height of a displayed voxel.
+    # If we are displaying in real world space,
+    # we use the world display resolution
+    if transform in ('affine'):
+
+        xpixdim = worldRes
+        ypixdim = worldRes
+
+    # But if we're just displaying the data (the
+    # transform is 'id' or 'pixdim'), we display
+    # it in the resolution of said data.
+    else:
+        xpixdim = image.pixdim[xax] * voxelRes
+        ypixdim = image.pixdim[yax] * voxelRes
+
+    # Number of samples across each dimension,
+    # given the current sample rate
+    xNumSamples = np.floor((xmax - xmin) / xpixdim)
+    yNumSamples = np.floor((ymax - ymin) / ypixdim)
+
+    # the adjusted width/height of our sample points
+    xpixdim = (xmax - xmin) / xNumSamples
+    ypixdim = (ymax - ymin) / yNumSamples
+
+    log.debug('Generating coordinate buffers for {} '
+              '({} resolution {}/{}, num samples {})'.format(
+                  image.name, transform, worldRes, voxelRes,
+                  xNumSamples * yNumSamples))
+
+    # The location of every displayed
+    # point in real world space
+    worldX = np.linspace(xmin + 0.5 * xpixdim,
+                         xmax - 0.5 * xpixdim,
+                         xNumSamples)
+    worldY = np.linspace(ymin + 0.5 * ypixdim,
+                         ymax - 0.5 * ypixdim,
+                         yNumSamples)
+
+    worldX, worldY = np.meshgrid(worldX, worldY)
+    
+    coords = np.zeros((worldX.size, 3))
+    coords[:, xax] = worldX.flatten()
+    coords[:, yax] = worldY.flatten()
+
+    return coords, xpixdim, ypixdim
