2806c06db7ff2c2b184e0ba775856ee0cd0da960
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-11-28 09:52:10 +0000

    Removed control/config stuff from FSLViewFrame

--- fsl/fslview/fslviewframe.py
+++ fsl/fslview/fslviewframe.py
@@ -53,9 +53,6 @@ class FSLViewFrame(wx.Frame):
         
         self._imageList  = imageList
         self._displayCtx = displayCtx
-        self._auimgr     = aui.AuiManager(self)
-
-        self._auimgr.SetDockSizeConstraint(50, 50)
 
         self._centrePane = aui.AuiNotebook(
             self,
@@ -64,27 +61,11 @@ class FSLViewFrame(wx.Frame):
             aui.AUI_NB_TAB_MOVE |
             aui.AUI_NB_CLOSE_ON_ALL_TABS)
 
-        paneInfo = (aui.AuiPaneInfo()
-                    .CentrePane()
-                    .Name('centre pane'))
-
-        self._auimgr.AddPane(self._centrePane, paneInfo)
-        self._auimgr.Update()
-
         # we can have as many view
         # panels as we like
         self._viewPanels      = []
         self._viewPanelTitles = {}
         self._viewPanelCount  = 0
-
-        # only one of each type of
-        # control panel is allowed
-        self._controlPanels = set()
-
-        # only one view config panel
-        # for each view panel that
-        # exists
-        self._viewConfigPanels = {}
 
         self._makeMenuBar()
         self._restoreState(default)
@@ -113,7 +94,6 @@ class FSLViewFrame(wx.Frame):
                            childDC) 
 
         self._viewPanelCount = self._viewPanelCount + 1
-        title = '{} {}'.format(title, self._viewPanelCount)
 
         self._viewPanels.append(panel)
         self._viewPanelTitles[id(panel)] = title
@@ -121,119 +101,6 @@ class FSLViewFrame(wx.Frame):
         self._centrePane.Split(
             self._centrePane.GetPageIndex(panel),
             wx.RIGHT)
-
-        if panel.hasConfigOptions():
-
-            menuText     = strings.viewPanelConfigMenuText[panelCls]
-            menuText     = menuText.format(title)
-            configAction = self._viewMenu.Append(wx.ID_ANY, menuText)
-        
-            def onConfig(ev):
-                self._addViewConfigPanel(panel, title)        
-
-            def onDestroy(ev):
-                ev.Skip()
-                try: self._viewMenu.RemoveItem(configAction)
-                except wx._core.PyDeadObjectError: pass
-
-            self .Bind(wx.EVT_MENU,           onConfig, configAction)
-            panel.Bind(wx.EVT_WINDOW_DESTROY, onDestroy)
-            
-
-    def _addViewConfigPanel(self, viewPanel, title):
-        """
-        """
-
-        if id(viewPanel) in self._viewConfigPanels.keys():
-            return
-        
-        confPanel = props.buildGUI(self, viewPanel)
-        paneInfo = (aui.AuiPaneInfo()
-                    .Float()
-                    .Dockable(True)
-                    .Floatable(True)
-                    .Movable(True)
-                    .CloseButton(True)
-                    .DestroyOnClose(True)
-                    .Gripper(False)
-                    .MaximizeButton(False)
-                    .MinimizeButton(False)
-                    .PinButton(False)
-                    .Caption(title)
-                    .CaptionVisible(True)
-                    .BestSize(confPanel.GetBestSize())
-                    .Name('config {}'.format(title)))
-        
-        self._auimgr.AddPane(confPanel, paneInfo)
-        self._auimgr.Update()
-
-        self._viewConfigPanels[id(viewPanel)] = confPanel
-
-        def onViewPanelDestroy(ev):
-            ev.Skip()
-
-            # if the config panel has already been closed, it
-            # will have been replaced with a python wrapper
-            # around a deleted C++ object. When converted to
-            # boolean, this wrapper will evaluate to False.
-            if not confPanel:
-                return
-            
-            self._auimgr.DetachPane(confPanel)
-            self._auimgr.Update()
-            confPanel.Destroy()
-
-        def onConfPanelDestroy(ev):
-            ev.Skip()
-            self._viewConfigPanels.pop(id(viewPanel))
-
-        viewPanel.Bind(wx.EVT_WINDOW_DESTROY, onViewPanelDestroy)
-        confPanel.Bind(wx.EVT_WINDOW_DESTROY, onConfPanelDestroy)
-                    
-
-    def addControlPanel(self, panelCls):
-        """Adds the given panel to the :class:`~wx.aui.AuiManager`."""
-
-        # If the specified control panel is
-        # already open, don't open a new one.
-        if panelCls in self._controlPanels:
-            return
-
-        title = strings.controlPanelTitles[panelCls]
-        panel = panelCls(self, self._imageList, self._displayCtx)
-            
-        paneInfo = (aui.AuiPaneInfo()
-                    .Float()
-                    .Dockable(True)
-                    .Floatable(True)
-                    .Movable(True)
-                    .CloseButton(True)
-                    .DestroyOnClose(True)
-                    .Gripper(False)
-                    .MaximizeButton(False)
-                    .MinimizeButton(False)
-                    .PinButton(False)
-                    .Caption(title)
-                    .CaptionVisible(True)
-                    .BestSize(panel.GetBestSize())
-                    .Name('control {}'.format(panel.__class__.__name__)))
-                    
-        self._auimgr.AddPane(panel, paneInfo)
-        self._auimgr.Update()
-
-        self._controlPanels.add(panelCls)
-
-        def onDestroy(ev):
-            ev.Skip()
-
-            # this method seems to be called when children of the
-            # target panel are destroyed, and we don't want that.
-            # So we'll just double check to make sure it is *this*
-            # panel which is being destroyed.
-            if ev.GetEventObject() == panel:
-                self._controlPanels.remove(panelCls)
-
-        panel.Bind(wx.EVT_WINDOW_DESTROY, onDestroy)
 
         
     def _onClose(self, ev):
@@ -249,15 +116,12 @@ class FSLViewFrame(wx.Frame):
 
         size     = self.GetSize().Get()
         position = self.GetScreenPosition().Get()
-        layout   = self._auimgr.SavePerspective()
 
         log.debug('Saving size: {}'    .format(str(size)))
         log.debug('Saving position: {}'.format(str(position)))
-        log.debug('Saving layout: {}'  .format(str(layout)))
 
         config.Write('size',     str(size))
         config.Write('position', str(position))
-        config.Write('layout',   layout)
 
         
     def _parseSavedSize(self, size):
@@ -312,15 +176,11 @@ class FSLViewFrame(wx.Frame):
         
         size     = None
         position = None
-        layout   = None
-        panels   = []
 
         if not default:
             config   = wx.Config('FSLView')
             size     = self._parseSavedSize(  config.Read('size'))
             position = self._parseSavedPoint( config.Read('position'))
-            layout   = config.Read('layout')
-            panels   = self._parseSavedLayout(layout)
 
         if size is not None:
             log.debug('Restoring previous size: {}'.format(size))
@@ -333,20 +193,6 @@ class FSLViewFrame(wx.Frame):
             self.SetPosition(position)
         else:
             self.Centre()
-
-        if layout is not None:
-            log.debug('Restoring previous layout: {}'.format(layout))
-
-            for panel in panels:
-
-                try:    panelType, panelClsName = panel.split()
-                except: continue
-
-                if panelType == 'control':
-                    panelCls  = getattr(controls, panelClsName)
-                    self.addControlPanel(panelCls)
-
-            self._auimgr.LoadPerspective(layout)
     
 
     def _makeMenuBar(self):
@@ -363,15 +209,10 @@ class FSLViewFrame(wx.Frame):
         viewMenu = wx.Menu()
         menuBar.Append(viewMenu, 'View')
 
-        ctrlMenu = wx.Menu()
-        menuBar.Append(ctrlMenu, 'Control') 
-
         self._fileMenu = fileMenu
         self._viewMenu = viewMenu
-        self._ctrlMenu = ctrlMenu
 
         viewPanels = views   .listViewPanels()
-        ctrlPanels = controls.listControlPanels()
         actionz    = actions .listActions()
 
         for action in actionz:
@@ -392,10 +233,3 @@ class FSLViewFrame(wx.Frame):
             self.Bind(wx.EVT_MENU,
                       lambda ev, vp=viewPanel: self.addViewPanel(vp),
                       viewAction)
-
-        for ctrlPanel in ctrlPanels:
-            ctrlAction = ctrlMenu.Append(wx.ID_ANY,
-                                         strings.controlPanelTitles[ctrlPanel]) 
-            self.Bind(wx.EVT_MENU,
-                      lambda ev, cp=ctrlPanel: self.addControlPanel(cp),
-                      ctrlAction)
