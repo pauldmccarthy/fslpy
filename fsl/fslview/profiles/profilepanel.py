51b0081e9e42ffccc6418ada63eea8c4457879dd
Paul McCarthy <pauld.mccarthy@gmail.com>
2014-12-16 17:57:57 +0000

    ProfilePanel now displays buttons for any defined Profile actions

--- fsl/fslview/profiles/profilepanel.py
+++ fsl/fslview/profiles/profilepanel.py
@@ -17,7 +17,11 @@ class ProfilePanel(wx.Panel):
     def __init__(self, parent, profile):
         wx.Panel.__init__(self, parent)
 
-        self._profile = profile
+        self._profile     = profile
+        self._propPanel   = wx.Panel(self)
+        self._actionPanel = wx.Panel(self)
+        self._actionSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._sizer       = wx.BoxSizer(wx.VERTICAL)
 
         propNames, _ = profile.getAllProperties()
 
@@ -25,4 +29,23 @@ class ProfilePanel(wx.Panel):
 
         view = props.HGroup(['mode'] + propNames)
 
-        props.buildGUI(self, profile, view)
+        props.buildGUI(self._propPanel, profile, view)
+
+        actions = profile.getActions()
+
+        for name, (label, func) in actions.items():
+
+            btn = wx.Button(self._actionPanel, label=label)
+            self._actionSizer.Add(btn)
+            btn.Bind(wx.EVT_BUTTON, lambda ev, f=func: f())
+
+
+        self._sizer.Add(self._propPanel)
+        self._sizer.Add(self._actionPanel)
+
+        self._actionPanel.SetSizer(self._actionSizer)
+        self             .SetSizer(self._sizer)
+
+
+        self._actionPanel.Layout()
+        self             .Layout()
