8971d18840b65f32ba3caae5462770521ccb3e57
Paul McCarthy <pauld.mccarthy@gmail.com>
2015-04-10 15:00:20 +0100

    Performance can be set via command line flag

--- fsl/tools/fslview_parseargs.py
+++ fsl/tools/fslview_parseargs.py
@@ -87,7 +87,7 @@ OPTIONS = td.TypeDict({
                        'showColourBar',
                        'colourBarLocation',
                        'colourBarLabelSide',
-                       'twoStageRender'],
+                       'performance'],
 
     # From here on, all of the keys are
     # the names of HasProperties classes,
@@ -172,7 +172,7 @@ ARGUMENTS = td.TypeDict({
     'SceneOpts.colourBarLocation'  : ('cbl', 'colourBarLocation'),
     'SceneOpts.colourBarLabelSide' : ('cbs', 'colourBarLabelSide'),
     'SceneOpts.showCursor'         : ('hc',  'hideCursor'),
-    'SceneOpts.twoStageRender'     : ('tr',  'twoStageRender'),
+    'SceneOpts.performance'        : ('p',   'performance'),
     
     'OrthoOpts.xzoom'       : ('xz', 'xzoom'),
     'OrthoOpts.yzoom'       : ('yz', 'yzoom'),
@@ -242,7 +242,8 @@ HELP = td.TypeDict({
     'SceneOpts.showColourBar'      : 'Show colour bar',
     'SceneOpts.colourBarLocation'  : 'Colour bar location',
     'SceneOpts.colourBarLabelSide' : 'Colour bar label orientation',
-    'SceneOpts.twoStageRender'     : 'Enable two-stage rendering',
+    'SceneOpts.performance'        : 'Rendering performance '
+                                     '(1=fastest, 5=best looking)',
     
     'OrthoOpts.xzoom'       : 'X canvas zoom',
     'OrthoOpts.yzoom'       : 'Y canvas zoom',
