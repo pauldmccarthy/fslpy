GLLineVector supporterd in affine display mode. Bugfix to LightBoxCanvas minimum slice spacing calculation.

fsl/fslview/gl/gl21/gllinevector_funcs.py
@@ -9,6 +9,7 @@ import numpy                  as np
 import OpenGL.GL              as gl
 import OpenGL.raw.GL._types   as gltypes
 
+import fsl.utils.transform    as transform
 import fsl.fslview.gl.shaders as shaders
 
 
@@ -112,33 +113,28 @@ def preDraw(self):
     
     gl.glEnableVertexAttribArray(self.vertexPos)
 
-    voxToDisplayMat = self.display.getTransform('voxel', 'display')
-    voxToDisplayMat = np.array(voxToDisplayMat, dtype=np.float32).ravel('C')
-
-    gl.glMatrixMode(gl.GL_MODELVIEW)
-    gl.glPushMatrix()
-    gl.glMultMatrixf(voxToDisplayMat)
-
     
 def draw(self, zpos, xform=None):
 
     opts    = self.opts
     indices = self.generateVertexIndices(zpos)
 
-    if xform is not None:
-        gl.glPushMatrix()
-        gl.glMultMatrixf(xform.ravel('C'))
-    
+    v2d = self.display.getTransform('voxel', 'display')
+
+    if xform is None: xform = v2d
+    else:             xform = transform.concat(v2d, xform)
+ 
+    gl.glPushMatrix()
+    gl.glMultMatrixf(np.array(xform, dtype=np.float32).ravel('C'))
+        
     gl.glLineWidth(opts.lineWidth)
     gl.glDrawElements(gl.GL_LINES, indices.size, gl.GL_UNSIGNED_INT, indices)
 
-    if xform is not None:
-        gl.glPopMatrix()
+    gl.glPopMatrix()
 
 
 def drawAll(self, zposes, xforms):
 
-    # TODO a proper implementation
     for zpos, xform in zip(zposes, xforms):
         self.draw(zpos, xform)
 
@@ -147,6 +143,3 @@ def postDraw(self):
     gl.glUseProgram(0)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
     gl.glDisableVertexAttribArray(self.vertexPos)
-
-    gl.glMatrixMode(gl.GL_MODELVIEW)
-    gl.glPopMatrix()

fsl/fslview/gl/gllinevector.py
@@ -8,6 +8,8 @@
 import logging
 
 import numpy                   as np
+
+import fsl.utils.transform     as transform
 import fsl.fslview.gl          as fslgl
 import fsl.fslview.gl.globject as globject
 import fsl.fslview.gl.glvector as glvector
@@ -179,7 +181,7 @@ class GLLineVector(glvector.GLVector):
         xax     = self.xax
         yax     = self.yax
         zax     = self.zax
-        
+
         if display.transform in ('id', 'pixdim'):
 
             if display.transform == 'pixdim':
@@ -195,25 +197,47 @@ class GLLineVector(glvector.GLVector):
             indices[yax] = np.arange(image.shape[yax], dtype=np.uint32)
             indices[zax] = np.array([zpos],            dtype=np.uint32)
 
-            indices = cartesian((indices[0],
-                                 indices[1],
-                                 indices[2],
-                                 [0, 1],
-                                 [0]))
-
+            indices = cartesian(indices)
             indices = np.ravel_multi_index((indices[:, 0],
                                             indices[:, 1],
-                                            indices[:, 2],
-                                            indices[:, 3],
-                                            indices[:, 4]),
-                                           self.voxelVertices.shape,
+                                            indices[:, 2]),
+                                           self.voxelVertices.shape[:3],
                                            order='C')
 
-            indices = np.array(indices, dtype=np.uint32) / 3
-
         else:
-            print 'Too hard for now!'
-            indices = np.array([], dtype=np.uint32)
+
+            lo, hi = display.getDisplayBounds()
+
+            if zpos <= lo[self.zax] or zpos >= hi[self.zax]:
+                return np.array([], dtype=np.uint32)
+
+            # sample a plane at the given display
+            # coordinate system zpos, through the
+            # image coordinate system space
+
+            # TODO change calcSamPoin signature
+            indices = globject.calculateSamplePoints(
+                image, display, xax, yax)[0]
+
+            indices[:, zax] = zpos
+
+            # transform to voxel space
+            indices = transform.transform(
+                indices,
+                display.getTransform('display', 'voxel'))
+
+            indices = np.array(indices + 0.5, dtype=np.uint32)
+
+            # flatten to 1D indices
+            indices = np.ravel_multi_index((indices[:, 0],
+                                            indices[:, 1],
+                                            indices[:, 2]),
+                                           self.voxelVertices.shape[:3],
+                                           order='C')
+
+        indices        = (indices * 2).repeat(2)
+        indices[1::2] += 1 
+        indices = np.array(indices, dtype=np.uint32)
 
         return indices
 