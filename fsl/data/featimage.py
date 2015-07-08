Bug fix to contrast vector normalisation - sum of squares, not squared sum. 'Current time course' settings only shown if current time course is being shown. Other little changes.

fsl/data/featimage.py
@@ -210,7 +210,7 @@ class FEATImage(fslimage.Image):
 
         if not fullmodel:
             contrast  = np.array(contrast)
-            contrast /= np.sqrt(contrast.sum() ** 2)
+            contrast /= np.sqrt((contrast ** 2).sum())
 
         x, y, z = xyz
         numEVs  = self.numEVs()