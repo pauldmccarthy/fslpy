ModelOpts bounds were incorrectly ordered - sometimes low bounds could be > high bounds, causing rendering/bounds calculation problems. Other minor fixes.

fsl/data/model.py
@@ -54,7 +54,6 @@ def loadVTKPolydataFile(infile):
 
         indexOffset        += polygonLengths[i]
 
-
     return vertices, polygonLengths, indices
     
 
@@ -84,6 +83,9 @@ class Model(object):
         self.vertices = np.array(data, dtype=np.float32)
         self.indices  = indices
 
+        self.__loBounds = self.vertices.min(axis=0)
+        self.__hiBounds = self.vertices.max(axis=0)
+
         log.memory('{}.init ({})'.format(type(self).__name__, id(self)))
 
         
@@ -101,5 +103,4 @@ class Model(object):
 
 
     def getBounds(self):
-        return (self.vertices.min(axis=0),
-                self.vertices.max(axis=0))
+        return (self.__loBounds, self.__hiBounds)

fsl/fslview/gl/glmodel.py
@@ -229,8 +229,9 @@ class GLModel(globject.GLObject):
         # the cross-section mask will not be created
         # correctly.
         direction = [gl.GL_INCR, gl.GL_DECR]
-        if np.any(hi < lo): faceOrder = [gl.GL_FRONT, gl.GL_BACK]
-        else:               faceOrder = [gl.GL_BACK,  gl.GL_FRONT]
+
+        if np.any(np.array(hi) < 0.0): faceOrder = [gl.GL_FRONT, gl.GL_BACK]
+        else:                          faceOrder = [gl.GL_BACK,  gl.GL_FRONT]
         
         for face, direction in zip(faceOrder, direction):
             