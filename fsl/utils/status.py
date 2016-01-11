e7ec6b1619b16a7d0f3061e8ed8ae87d1c0e8ddb
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-01-11 19:17:53 +0000

    Status is automatically cleared after a specified timeout.

--- fsl/utils/status.py
+++ fsl/utils/status.py
@@ -13,7 +13,7 @@ following functions:
 
     setTarget
     update
-    clear
+    clearStatus
 
 
 The :func:`update` function may be used to display a message. By default, the
@@ -23,16 +23,24 @@ passed to this target.
 """
 
 
+import            threading
 import            logging
 import            inspect
 import os.path as op
 
+
 log = logging.getLogger(__name__)
 
 
-statusUpdateTarget = None
+_statusUpdateTarget = None
 """A reference to the status update target - this is ``None`` by default, and
 can be set via :func:`setTarget`.
+"""
+
+
+_clearThread = None
+"""Reference to a :class:`ClearThread`, which is a daemon thread that clears
+the status after the timeout passed to the :func:`update` function. 
 """
 
 
@@ -40,16 +48,21 @@ def setTarget(target):
     """Set a target function to receive status updates. The ``target`` must
     be a function which accepts a string as its sole parameter.
     """
-    global statusUpdateTarget
-    statusUpdateTarget = target
+    global _statusUpdateTarget
+    _statusUpdateTarget = target
 
 
-def update(message):
+def update(message, timeout=1.0):
     """Display a status update to the user. The message is logged and,
     if a status update target has been set, passed to the target.
+
+    :arg timeout: Timeout (in seconds) after which the status will be
+                  cleared (via the :class:`ClearThread`). Pass in ``None``
+                  to disable this behaviour.
     """
 
-    global statusUpdateTarget
+    global _clearThread
+    global _statusUpdateTarget
 
     if log.getEffectiveLevel() == logging.DEBUG:
         
@@ -60,17 +73,71 @@ def update(message):
 
         log.debug('[{}:{}] {}'.format(module, linenum, message))
 
-    if statusUpdateTarget is None:
+    if _statusUpdateTarget is None:
         return
+
+    _statusUpdateTarget(message)
+
+    if timeout is not None:
         
-    statusUpdateTarget(message)
+        if _clearThread is None:
+            _clearThread = ClearThread()
+            _clearThread.start()
+
+        _clearThread.clear(timeout)
 
 
-def clear():
+def clearStatus():
     """Clear the status. If a status update target has been set, it is passed
     the empty string.
     """ 
-    if statusUpdateTarget is None:
+    if _statusUpdateTarget is None:
         return
         
-    statusUpdateTarget('') 
+    _statusUpdateTarget('') 
+
+    
+class ClearThread(threading.Thread):
+    """The ``ClearThread`` is a daemon thread used by the :func:`update`
+    function. Only one ``ClearThread`` is ever started - it is started on the
+    first call to ``update`` when a timeout is specified.
+
+    The ``ClearThread`` waits until the :meth:`clear` method is called.
+    It then waits for the specified timeout and, unless another call to
+    :meth:`clear` has been made, clears the status via a call to
+    :func:`clearStatus`.
+
+    Apologies for the confusing naming.
+    """
+
+    
+    def __init__(self):
+        """Create a ``ClearThread``. """
+
+        threading.Thread.__init__(self)
+
+        self.daemon       = True
+        self.__clearEvent = threading.Event()
+        self.__timeout    = None
+
+        
+    def clear(self, timeout):
+        """Clear the status after the specified timeout (in seconds). """
+        
+        self.__timeout = timeout
+        self.__clearEvent.set()
+
+        
+    def run(self):
+        """The ``ClearThread`` function. Infinite loop which waits until
+        the :meth:`clear` method is called, and then clears the status
+        (via a call to :func:`clearStatus`).
+        """
+
+        while True:
+
+            self.__clearEvent.wait()
+            self.__clearEvent.clear()
+
+            if not self.__clearEvent.wait(self.__timeout):
+                clearStatus()
