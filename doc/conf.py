bf0a8cf18d33a8ceccfb265360aa384d7387b464
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-08-23 14:56:48 +0100

    Documentation built for private things

--- doc/conf.py
+++ doc/conf.py
@@ -346,7 +346,7 @@ epub_exclude_files = ['search.html']
 autoclass_content = 'class'
 
 # Document private members and special members (e.g. __init__)
-autodocsourc_default_flags = ['special-members']
+autodocsourc_default_flags = ['private-members', 'special-members']
 
 # Documentation for python modules is in the same order
 # as the source code.
