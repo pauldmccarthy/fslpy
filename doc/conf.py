00b7e95cba2d095a2a6287bbbfe48c4dc40c043a
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-11-04 15:01:01 +0000

    Univ Oxford in copyright notice

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,8 @@ master_doc = 'fsl'
 
 # General information about the project.
 project = u'fslpy'
-copyright = u'{}, Paul McCarthy, FMRIB Centre'.format(date.year)
+copyright = u'{}, Paul McCarthy, FMRIB Centre, University of Oxford'.format(
+    date.year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
