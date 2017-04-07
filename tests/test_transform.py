5d431a9f455e0fb643d74ef282fe2eb393a90a88
Paul McCarthy <pauld.mccarthy@gmail.com>
2017-04-07 17:34:34 +0100

    Nearly 100% coverage for transform module

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -5,9 +5,12 @@
 # Author: Paul McCarthy <pauldmccarthy@gmail.com>
 #
 
-import os.path as op
+import              glob
+import os.path   as op
+import itertools as it
+import numpy     as np
 
-import numpy as np
+import pytest
 
 import fsl.utils.transform as transform
 
@@ -108,7 +111,7 @@ def test_compose_and_decompose():
         scales, offsets, rotations = transform.decompose(xform)
         result = transform.compose(scales, offsets, rotations)
 
-        assert np.all(np.isclose(xform, result))
+        assert np.all(np.isclose(xform, result, atol=1e-5))
 
         # The decompose function does not support a
         # different rotation origin, but we test
@@ -117,4 +120,136 @@ def test_compose_and_decompose():
         scales, offsets, rotations = transform.decompose(xform)
         result = transform.compose(scales, offsets, rotations, [0, 0, 0])
 
-        assert np.all(np.isclose(xform, result))
+        assert np.all(np.isclose(xform, result, atol=1e-5))
+
+
+def test_axisBounds():
+    testfile = op.join(datadir, 'test_transform_test_axisBounds.txt')
+    lines    = readlines(testfile)
+    ntests   = len(lines) / 6
+
+    def readTest(testnum):
+        tlines   = lines[testnum * 6: testnum * 6 + 6]
+        params   = [p.strip() for p in tlines[0].split(',')]
+        shape    = [int(s) for s in params[0].split()]
+        origin   = params[1]
+        boundary = None if params[2] == 'None' else params[2]
+        xform    = np.genfromtxt(tlines[1:5])
+        expected = np.genfromtxt([tlines[5]])
+        expected = (expected[:3], expected[3:])
+
+        return shape, origin, boundary, xform, expected
+        
+    allAxes  = list(it.chain(
+        range(0, 1, 2),
+        it.permutations((0, 1, 2), 1),
+        it.permutations((0, 1, 2), 2),
+        it.permutations((0, 1, 2), 3)))
+
+    for i in range(ntests):
+
+        shape, origin, boundary, xform, expected = readTest(i)
+
+        print('Test       {}'.format(i))
+        print('  shape    {}'.format(shape))
+        print('  origin   {}'.format(origin))
+        print('  boundary {}'.format(boundary))
+
+        for axes in allAxes:
+            result = transform.axisBounds(shape,
+                                          xform,
+                                          axes=axes,
+                                          origin=origin,
+                                          boundary=boundary)
+
+            exp = expected[0][(axes,)], expected[1][(axes,)]
+
+            assert np.all(np.isclose(exp, result))
+
+
+    # Do some parameter checks on 
+    # the first test in the file
+    # which has origin == centre
+    for i in range(ntests):
+        shape, origin, boundary, xform, expected = readTest(i)
+        if origin == 'centre':
+            break
+
+    # US-spelling
+    assert np.all(np.isclose(
+        expected,
+        transform.axisBounds(
+            shape, xform, origin='center', boundary=boundary)))
+
+    # Bad origin/boundary values
+    with pytest.raises(ValueError):
+        transform.axisBounds(shape, xform, origin='Blag', boundary=boundary)
+    with pytest.raises(ValueError):
+        transform.axisBounds(shape, xform, origin=origin, boundary='Blufu') 
+
+
+def test_transform():
+
+    def is_orthogonal(xform):
+        """Returns ``True`` if the given xform consists
+        solely of translations and scales.
+        """
+
+        mask = np.array([[1, 0, 0, 1],
+                         [0, 1, 0, 1],
+                         [0, 0, 1, 1],
+                         [0, 0, 0, 1]], dtype=np.bool)
+
+        return np.all((xform != 0) == mask)
+
+    coordfile   = op.join(datadir, 'test_transform_test_transform_coords.txt')
+    testcoords  = np.loadtxt(coordfile)
+
+    testpattern = op.join(datadir, 'test_transform_test_transform_??.txt')
+    testfiles   = glob.glob(testpattern)
+
+    allAxes  = list(it.chain(
+        range(0, 1, 2),
+        it.permutations((0, 1, 2), 1),
+        it.permutations((0, 1, 2), 2),
+        it.permutations((0, 1, 2), 3)))
+    
+    for i, testfile in enumerate(testfiles):
+        
+        lines    = readlines(testfile)
+        xform    = np.genfromtxt(lines[:4])
+        expected = np.genfromtxt(lines[ 4:])
+        result   = transform.transform(testcoords, xform)
+ 
+        assert np.all(np.isclose(expected, result))
+
+        if not is_orthogonal(xform):
+            continue
+            
+        for axes in allAxes:
+            atestcoords = testcoords[:, axes]
+            aexpected   = expected[  :, axes]
+            aresult     = transform.transform(atestcoords, xform, axes=axes)
+
+            assert np.all(np.isclose(aexpected, aresult))
+
+    # Pass in some bad data, expect an error
+    xform     = np.eye(4)
+    badxform  = np.eye(3)
+    badcoords = np.random.randint(1, 10, (10, 4))
+    coords    = badcoords[:, :3]
+
+    with pytest.raises(IndexError):
+        transform.transform(coords, badxform) 
+
+    with pytest.raises(ValueError):
+        transform.transform(badcoords, xform)
+        
+    with pytest.raises(ValueError):
+        transform.transform(badcoords.reshape(5, 2, 4), xform)
+
+    with pytest.raises(ValueError):
+        transform.transform(badcoords.reshape(5, 2, 4), xform, axes=1) 
+
+    with pytest.raises(ValueError):
+        transform.transform(badcoords[:, (1, 2, 3)], xform, axes=[1, 2])
