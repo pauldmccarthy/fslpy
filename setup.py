c5c9f7507f4dafd2bb43f48f929f5ce4114c66b5
Paul McCarthy <pauld.mccarthy@gmail.com>
2017-04-20 16:28:13 +0100

    Tests now depend on mock

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     install_requires=install_requires,
 
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-cov', 'pytest-runner'],
+    tests_require=['pytest', 'mock', 'pytest-cov', 'pytest-runner'],
     test_suite='tests',
 
     cmdclass={'doc' : doc},
