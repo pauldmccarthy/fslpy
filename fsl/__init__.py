cc43108006efa9e7608db5577a1215109e75493a
Paul McCarthy <pauldmccarthy@gmail.com>
2017-08-28 08:54:30 -0700

    __init__.py is back. Pkgutil-style namespace packages aren't too bad, and give py2/3 compatibility

---
+++ fsl/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# __init__.py -
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""The :mod:`fsl` package is a library which contains convenience classes
+and functions for use by FSL python tools. It is broadly split into the
+following sub-packages:
+
+.. autosummary::
+
+   fsl.data
+   fsl.utils
+   fsl.scripts
+   fsl.version
+
+.. note:: The ``fsl`` namespace is a ``pkgutil``-style *namespace package* -
+          it can be used across different projects - see
+          https://packaging.python.org/guides/packaging-namespace-packages/
+          for details.
+"""
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
