fe8f32274c1bff498a5daaf4838b30b75c3e7c23
Paul McCarthy <pauldmccarthy@gmail.com>
2017-11-07 18:39:22 +0000

    imglob made into a standalone function

--- fsl/scripts/imglob.py
+++ fsl/scripts/imglob.py
@@ -26,9 +26,62 @@ exts   = fslimage.ALLOWED_EXTENSIONS
 groups = fslimage.FILE_GROUPS
 
 
+def imglob(paths, output=None):
+    """Given a list of file names, identifies and returns the unique
+    NIFTI/ANALYZE image files that exist.
+
+    :arg paths:  Sequence of paths/prefixes to glob.
+
+    :arg output: One of ``'prefix'`` (the default), ``'all'``, or
+                 ``'primary'``:
+
+                  - ``'prefix'``:  Returns the files without extensions.
+                  - ``'all'``:     Returns all files that match (e.g. both
+                                   ``.img`` and ``.hdr`` files will be
+                                   returned).
+                  - ``'primary'``: Returns only the primary file of each
+                                   matching file group, e.g. only the
+                                   ``.hdr`` file would be returned from
+                                   an ``.img``/``.hdr`` pair.
+
+    :returns: A sequence of resolved path names, in the form specified
+              by the ``output`` parameter.
+    """
+
+    if output is None:
+        output = 'prefix'
+
+    if output not in ('prefix', 'all', 'primary'):
+        raise ValueError('Unsupported output format: {}'.format(output))
+
+    imgfiles = []
+
+    # Build a list of all image files (both
+    # hdr and img and otherwise) that match
+    for path in paths:
+        try:
+            path = fslimage.removeExt(path)
+            imgfiles.extend(fslimage.addExt(path, unambiguous=False))
+        except fslpath.PathError:
+            continue
+
+    if output == 'prefix':
+        imgfiles = fslpath.removeDuplicates(imgfiles,
+                                            allowedExts=exts,
+                                            fileGroups=groups)
+        imgfiles = [fslpath.removeExt(f, exts) for f in imgfiles]
+
+    elif output == 'primary':
+        imgfiles = fslpath.removeDuplicates(imgfiles,
+                                            allowedExts=exts,
+                                            fileGroups=groups)
+
+    return list(sorted(set(imgfiles)))
+
+
 def main(argv=None):
-    """The ``imglob`` utility. Given a list of file names, identifies and prints
-    the unique NIFTI/ANALYZE image files.
+    """The ``imglob`` application. Given a list of file names, identifies and
+    prints the unique NIFTI/ANALYZE image files.
     """
 
     if argv is None:
@@ -45,30 +98,10 @@ def main(argv=None):
     if output == 'prefix': paths = argv
     else:                  paths = argv[1:]
 
-    imgfiles = []
+    imgfiles = imglob(paths, output)
 
-    # Build a list of all image files (both
-    # hdr and img and otherwise) that match
-    for path in paths:
-        try:
-            imgfiles.extend(fslimage.addExt(path, unambiguous=False))
-        except fslpath.PathError:
-            continue
-
-    if output == 'prefix':
-        imgfiles = fslpath.removeDuplicates(imgfiles,
-                                            allowedExts=exts,
-                                            fileGroups=groups)
-        imgfiles = [fslpath.removeExt(f, exts) for f in imgfiles]
-
-    elif output == 'primary':
-        imgfiles = fslpath.removeDuplicates(imgfiles,
-                                            allowedExts=exts,
-                                            fileGroups=groups)
-
-    imgfiles = sorted(set(imgfiles))
-
-    print(' '.join(imgfiles))
+    if len(imgfiles) > 0:
+        print(' '.join(imgfiles))
 
     return 0
 
