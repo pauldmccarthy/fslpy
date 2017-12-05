70af678605372a78cd7fe446ced90ef101e8cdb4
Paul McCarthy <pauldmccarthy@gmail.com>
2017-12-05 11:09:52 +1030

    Boolean masks cannot be used when image is kept on disk. Making unit tests pass, but this should probably be fixed.

--- tests/test_atlasq_ohi.py
+++ tests/test_atlasq_ohi.py
@@ -179,12 +179,7 @@ def test_mask(seed):
 
             # atlasquery always uses 2mm
             # resolution versions of atlases
-            atlas2mm = fslatlases.loadAtlas(
-                desc.atlasID,
-                resolution=2,
-                indexed=True,
-                calcRange=False,
-                loadData=False)
+            atlas2mm = fslatlases.loadAtlas(desc.atlasID, resolution=2)
 
             # Test with 1mm and 2mm masks
             for res in [1, 2]:
