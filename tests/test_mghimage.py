e3e8ca90cf308cb550c5d459751bad67778b5103
Paul McCarthy <pauldmccarthy@gmail.com>
2018-01-17 18:32:09 +0000

    Expanded mghimage unit test

--- tests/test_mghimage.py
+++ tests/test_mghimage.py
@@ -31,8 +31,16 @@ def test_MGHImage():
     datadir  = op.join(op.dirname(__file__), 'testdata')
     testfile = op.join(datadir, 'example.mgz')
 
+    # Load from a file
     img      = fslmgh.MGHImage(testfile)
     nbimg    = nib.load(testfile)
 
     assert np.all(np.isclose(img[:],            nbimg.get_data()))
     assert np.all(np.isclose(img.voxToWorldMat, nbimg.affine))
+    assert img.mghImageFile == testfile
+
+    # Load from an in-memory nibabel object
+    img = fslmgh.MGHImage(nbimg)
+    assert np.all(np.isclose(img[:],            nbimg.get_data()))
+    assert np.all(np.isclose(img.voxToWorldMat, nbimg.affine))
+    assert img.mghImageFile is None
