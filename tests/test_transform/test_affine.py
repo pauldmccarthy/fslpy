2920116b542e35cf58a833af88010c70e06c1e91
Paul McCarthy <pauldmccarthy@gmail.com>
2020-10-08 14:19:13 +0100

    TEST: Add test case with shear component

--- tests/test_transform/test_affine.py
+++ tests/test_transform/test_affine.py
@@ -226,8 +226,9 @@ def test_compose_and_decompose():
         xform                      = lines[i * 4: i * 4 + 4]
         xform                      = np.genfromtxt(xform)
 
-        scales, offsets, rotations = affine.decompose(xform)
-        result = affine.compose(scales, offsets, rotations)
+        scales, offsets, rotations, shears = affine.decompose(
+            xform, shears=True)
+        result = affine.compose(scales, offsets, rotations, shears=shears)
 
         assert np.all(np.isclose(xform, result, atol=1e-5))
 
