627d9900e2afbe32821e6973c682ffea8bb9cd83
Paul McCarthy <pauldmccarthy@gmail.com>
2023-07-23 11:40:06 +0100

    MNT: Automatically set sform for FNIRT displacement fields which do not have their sform set

---
+++ fsl/wrappers/miscvis.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# miscvis.py - Wrappers for commands from the miscvis package.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+
+import logging
+log = logging.getLogger(__name__)
