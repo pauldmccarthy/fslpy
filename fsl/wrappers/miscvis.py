298aabf4207efc79b49e3c98a801b9862028bfeb
Paul McCarthy <pauldmccarthy@gmail.com>
2023-07-26 10:26:23 +0100

    BF: Did not mean to add this

--- fsl/wrappers/miscvis.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#
-# miscvis.py - Wrappers for commands from the miscvis package.
-#
-# Author: Paul McCarthy <pauldmccarthy@gmail.com>
-#
-
-import logging
-log = logging.getLogger(__name__)
