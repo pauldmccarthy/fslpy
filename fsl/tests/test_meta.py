edf034f011ee4207782b43cf8f1f3822300d0abd
Paul McCarthy <pauldmccarthy@gmail.com>
2023-09-05 18:45:01 +0100

    TEST: Test Meta.meta property

--- fsl/tests/test_meta.py
+++ fsl/tests/test_meta.py
@@ -19,6 +19,8 @@ def test_meta():
     for k, v in data.items():
         assert m.getMeta(k) == v
 
+    assert m.meta == data
+
     assert list(data.keys())   == list(m.metaKeys())
     assert list(data.values()) == list(m.metaValues())
     assert list(data.items())  == list(m.metaItems())
