fb19343392fe464178eaffab3d82e06607b2ba53
Paul McCarthy <pauldmccarthy@gmail.com>
2024-03-05 15:34:35 +0000

    TEST: Awkward unit test for fot collision. Not particularly robust

--- fsl/tests/test_wrappers/test_concurrent.py
+++ fsl/tests/test_wrappers/test_concurrent.py
@@ -5,17 +5,28 @@ import glob
 import os
 import os.path as op
 import multiprocessing as mp
+import random
 import shutil
+import time
 
 from fsl.data.image import removeExt
 from fsl.utils.tempdir import tempdir
-from fsl.wrappers import bet
+import fsl.wrappers.wrapperutils as wutils
 
 
 datadir = op.join(op.dirname(__file__), '..', 'testdata')
 
 
+
+@wutils.fileOrImage('infile')
+@wutils.fileOrImage('outfile')
+def myfunc(infile, outfile):
+    time.sleep(1 + random.random() * 2)
+    shutil.copy(infile, outfile)
+
+# fsl/fslpy!446
 def test_wrappers_concurrent():
+
     exfunc = op.join(datadir, 'example_func.nii.gz')
     with tempdir():
         for i in range(1, 11):
@@ -24,10 +35,8 @@ def test_wrappers_concurrent():
         basenames = [removeExt(f) for f in infiles]
         outfiles  = [f'{bn}_brain.nii.gz' for bn in basenames]
 
-        os.environ['FSLOUTPUTTYPE'] = 'NIFTI_GZ'
-
         with mp.Pool(len(infiles)) as pool:
             args = zip(infiles, outfiles)
-            pool.starmap(bet, args)
+            pool.starmap(myfunc, args)
 
         assert all(op.exists(f) for f in outfiles)
