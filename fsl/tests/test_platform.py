bb3272335756c27ba1d33c71ac5eaa241184d873
Paul McCarthy <pauldmccarthy@gmail.com>
2024-05-24 10:33:59 +0100

    TEST: Smoke tests for platform properties

--- fsl/tests/test_platform.py
+++ fsl/tests/test_platform.py
@@ -28,13 +28,17 @@ def test_atts():
     p.haveGui
     p.canHaveGui
     p.inSSHSession
+    p.inVNCession
     p.wxPlatform
     p.wxFlavour
     p.fsldir
+    p.fsldevdir
     p.fslVersion
     p.glVersion
     p.glRenderer
     p.glIsSoftwareRenderer
+    p.wsl
+    p.fslwsl
 
 
 @pytest.mark.wxtest
