754c3547b1f711aa9de3aec9a2b961068c7524b3
Paul McCarthy <pauldmccarthy@gmail.com>
2025-11-11 16:33:33 +0000

    ENH: Add mmorf wrapper

---
+++ fsl/wrappers/mmorf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# mmorf.py - Wrapper for mmorf.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""This module provides wrapper functions for the FSL :fsldocs:`MMORF
+<registration/mmorf.html>` registration utility.
+"""
+
+
+from . import wrapperutils as wutils
+
+
+@wutils.fslwrapper
+def mmorf(config=None, **kwargs):
+    """Wrapper for the ``mmorf`` command. """
+
+    cmd = ['mmorf']
+
+    if config is not None:
+        cmd += ['--config', config]
+
+    cmd += wutils.applyArgStyle('--', kwargs)
+
+    return cmd
